da3acf05b13a42e36f6177b79cdda6aa18391993
taniyanegi <taniyanegi632@gmail.com>
2026-08-07 23:34:25 +0530

    push

---
+++ Patterns/Trees/CountLeaveNodes.cpp
@@ -0,0 +1,61 @@
+
+// count leaf nodes in a binary tree
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+
+Node* buildTree(vector<int>& preorder) {
+    static int idx = -1;
+    idx++;
+
+    if (preorder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
+
+
+
+int countLeaves(Node* root) {
+        // write code here
+          if(root==NULL){
+              return 0;
+          }
+        if(root->left==NULL && root->right==NULL){
+            return 1;
+        }
+        int leftCount=countLeaves(root->left);
+        int rightCount=countLeaves(root->right);
+        
+        return leftCount+rightCount;
+    }
+
+
+    int main(){
+        
+        // example usage
+        vector<int> preorder={1,2,-1,-1,3,4,-1,-1,3,-1,-1,5,-1,-1};
+         
+        Node* root=buildTree(preorder);
+
+        int Total_Leaves=countLeaves(root);
+        cout<<"total leaves are "<<Total_Leaves<<endl;
+        return 0;
+    }
